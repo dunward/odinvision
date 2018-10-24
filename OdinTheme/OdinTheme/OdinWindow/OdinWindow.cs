2f46c0716b1a142e950db28ac3aef17a2b6f1790
eglik <eglik@odin.vision>
2018-10-24 14:11:49 +0900

    feat: OdinTheme

---
+++ OdinTheme/OdinTheme/OdinWindow/OdinWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace OdinTheme
+{
+    public class OdinWindow : Window
+    {
+        public OdinWindow()
+        {
+            InitializeCommandBindings();
+            Style = FindResource(typeof(OdinWindow)) as Style;
+        }
+
+        static OdinWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(OdinWindow), new FrameworkPropertyMetadata(typeof(OdinWindow)));
+        }
+
+        #region Command Bindings
+        void InitializeCommandBindings()
+        {
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, CommandBinding_CloseExecuted));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, CommandBinding_MaximizeExecuted));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, CommandBinding_MinimizeExecuted));
+            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, CommandBinding_RestoreExecuted));
+        }
+
+        void CommandBinding_RestoreExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        void CommandBinding_MinimizeExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        void CommandBinding_MaximizeExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        void CommandBinding_CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+        #endregion
+    }
+}
---
+++ OdinTheme/OdinTheme/OdinWindow/Theme/OdinTheme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OdinTheme
+{
+    public class OdinTheme : ResourceDictionary
+    {
+        public OdinTheme()
+        {
+            Source = new Uri("pack://application:,,,/OdinTheme;component/OdinWindow/OdinThemeComponent.xaml");
+        }
+    }
+}
