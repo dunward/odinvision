a7fd0a0202d59f10299e1c1202f96428204e3e1d
EGLIK <eglik@odin.vision>
2019-04-06 18:51:40 +0900

    commit: MIT License

--- OdinTheme/WpfApp/MainWindow.xaml.cs
+++ OdinTheme/WpfApp/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class MainWindow : OdinWindow
     {
         public MainWindow()
